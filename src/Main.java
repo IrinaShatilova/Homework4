01318c4294acd66b419e9775201acff909633c9b
shati <shatilova_ira@mail.ru>
2022-11-14 16:50:06 +0600

    1 Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework4");
+        //Задание 1
+        System.out.println (" ");
+        System.out.println ("Задание 1");
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Тебе уже " + age + ". Поздравляю с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Ты еще не достиг совершеннолетия, придется немного подождать. Твой возраст - " + age);
+        }
+
     }
 }
